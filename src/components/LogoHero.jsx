74188a8c4f47cf6dbfa34d7cdff06ab95f3870ab
“Sam <scha4@gmu.edu>
2023-09-17 19:33:27 +0900

    logo changes

--- src/components/LogoHero.jsx
+++ src/components/LogoHero.jsx
@@ -2,28 +2,49 @@ import React from "react";
 
 function LogoHero() {
   return (
-    <div className="flex items-center justify-center gap-2 sm:gap-10 py-36 ">
-      <img
-        src="./KBLlogo.png"
-        alt="KBL Logo"
-        className="w-12 h-12 sm:w-24 sm:h-24 "
-      />
-      <img
-        src="./KNTlogo.png"
-        alt="KNT Logo"
-        className="w-12 h-12 sm:w-24 sm:h-24 "
-      />
-      <img
-        src="./NCAALOGO.png"
-        alt="NCAA Logo"
-        className="w-12 h-12 sm:w-24 sm:h-24 "
-      />
-      <img
-        src="./fibaLogo.png"
-        alt="FIBA Logo"
-        className="w-24 h-12 sm:w-36 sm:h-24 "
-      />
-    </div>
+    <>
+      <div className="py-36 ">
+        <div className="flex items-center justify-center gap-2 sm:gap-10 ">
+          <img
+            src="./KBLlogo.png"
+            alt="KBL Logo"
+            className="w-12 h-12 sm:w-24 sm:h-24 "
+          />
+          <img
+            src="./KNTlogo.png"
+            alt="KNT Logo"
+            className="w-12 h-12 sm:w-24 sm:h-24 "
+          />
+          <img
+            src="./NCAALOGO.png"
+            alt="NCAA Logo"
+            className="w-12 h-12 sm:w-24 sm:h-24 "
+          />
+          <img
+            src="./fibaLogo.png"
+            alt="FIBA Logo"
+            className="w-24 h-12 sm:w-36 sm:h-24 "
+          />
+        </div>
+        <div className="flex items-center justify-center gap-2 sm:gap-10 mt-10">
+          <img
+            src="./wkbllogo.jpg"
+            alt="WKBL Logo"
+            className="w-16 h-12 sm:w-32 sm:h-24 "
+          />
+          <img
+            src="./fiba3x3logo.jpg"
+            alt="WKBL Logo"
+            className="w-16 h-12 sm:w-32 sm:h-24 "
+          />
+          <img
+            src="./aaulogo.png"
+            alt="WKBL Logo"
+            className="w-12 h-12 sm:w-24 sm:h-24 "
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
